47d2009d2f7c0ae64dcda7125a3b0e4d53ee7c97
huiningd <wtning@gmail.com>
2018-04-24 20:29:55 +0300

    Rename Database to TodoDatabase, in which we create a single volatile instance of the database.

--- app/src/main/java/fi/huiningd/guaguatodo/data/Database.kt
+++
@@ -1,9 +0,0 @@
-package fi.huiningd.guaguatodo.data
-
-import android.arch.persistence.room.Database
-import android.arch.persistence.room.RoomDatabase
-
-@Database(entities = arrayOf(TodoItem::class), version = 1)
-abstract class Database : RoomDatabase() {
-    abstract fun todoItemDao(): TodoItemDao
-}
