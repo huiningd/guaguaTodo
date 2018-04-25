d53ff25081a5997848e6520fc52ef027f209e9fa
huiningd <wtning@gmail.com>
2018-04-25 21:38:34 +0300

    Add some comment.

--- app/src/main/java/fi/huiningd/guaguatodo/data/TodoItemDao.kt
+++ app/src/main/java/fi/huiningd/guaguatodo/data/TodoItemDao.kt
@@ -6,6 +6,17 @@ import io.reactivex.Flowable
 @Dao
 interface TodoItemDao {
 
+    /**
+     * Why use Flowable?
+     * When there is no data in the todoDatabase and the query returns no rows,
+     * the Flowable will not emit, neither onNext, nor onError.
+     * When there is a user in the todoDatabase, the Flowable will trigger onNext.
+     * Every time the data is updated, the Flowable object will emit
+     * automatically, allowing you to update the UI based on the latest data.
+     *
+     * see https://medium.com/google-developers/room-rxjava-acb0cd4f3757
+     */
+
     @Query("SELECT * FROM $TABLE_NAME")
     fun getAllTodoItems(): Flowable<List<TodoItem>>
 
