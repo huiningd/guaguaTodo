df57548824100312f60fe04a83c4c1906375e3d9
huiningd <wtning@gmail.com>
2018-04-25 21:49:38 +0300

    When user checks the check-box, strikes through the TODO text, and propagates the isDone property to database.

--- app/src/main/java/fi/huiningd/guaguatodo/TodoListRecyclerViewAdapter.kt
+++ app/src/main/java/fi/huiningd/guaguatodo/TodoListRecyclerViewAdapter.kt
@@ -1,6 +1,7 @@
 package fi.huiningd.guaguatodo
 
 import android.content.Intent
+import android.graphics.Paint
 import android.os.Bundle
 import android.support.v7.widget.RecyclerView
 import android.util.Log
@@ -10,20 +11,26 @@ import android.view.ViewGroup
 import android.widget.CheckBox
 import android.widget.TextView
 import fi.huiningd.guaguatodo.data.TodoItem
-import kotlinx.android.synthetic.main.todo_list_content.view.*
+import kotlinx.android.synthetic.main.todo_item_content.view.*
 
 
 class TodoListRecyclerViewAdapter(private val mParentActivity: TodoListActivity,
                                   private val mTwoPane: Boolean) :
         RecyclerView.Adapter<TodoListRecyclerViewAdapter.ViewHolder>() {
 
+    interface ListItemListener {
+        fun toggleDone(isDone: Boolean, item: TodoItem)
+        fun toggleStarred(isStarred: Boolean, item: TodoItem)
+    }
 
-    private val mOnClickListener: View.OnClickListener
+    private val TAG = "RecyclerViewAdapter"
+
+    private var mListItemListener : ListItemListener? = null
+    private val mDetailViewListener: View.OnClickListener
     private var mTodoList: MutableList<TodoItem> = ArrayList()
 
-
     init {
-        mOnClickListener = View.OnClickListener { v ->
+        mDetailViewListener = View.OnClickListener { v ->
             val item = v.tag as TodoItem
             if (mTwoPane) {
                 val fragment = TodoItemDetailFragment().apply {
@@ -42,21 +49,27 @@ class TodoListRecyclerViewAdapter(private val mParentActivity: TodoListActivity,
                 v.context.startActivity(intent)
             }
         }
+
+        try {
+            mListItemListener = mParentActivity
+        } catch (e: ClassCastException) {
+            throw ClassCastException(mParentActivity.toString()
+                    + " must implement " + mListItemListener.toString())
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context)
-                .inflate(R.layout.todo_list_content, parent, false)
+                .inflate(R.layout.todo_item_content, parent, false)
         return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = mTodoList[position]
-        holder.mTodoTitleView.text = item.title
 
         with(holder.itemView) {
             tag = item
-            setOnClickListener(mOnClickListener)
+            holder.bind(item, mListItemListener)
         }
     }
 
@@ -65,13 +78,42 @@ class TodoListRecyclerViewAdapter(private val mParentActivity: TodoListActivity,
     }
 
     fun updateList(list: List<TodoItem>) {
-        Log.e("###", "updateList")
+        //Log.e(TAG, "updateList")
         mTodoList = list.toMutableList()
-        notifyDataSetChanged()
+        notifyDataSetChanged() // TODO make comparison with the old list, only refresh single item
     }
 
-    inner class ViewHolder(mView: View) : RecyclerView.ViewHolder(mView) {
-        val mCheckBox: CheckBox = mView.check_box
-        val mTodoTitleView: TextView = mView.todo_title
+    private fun toggleTextStrikeThru(isChecked: Boolean, textView: TextView) {
+        if (isChecked) strikeThruText(textView)
+        else
+            textView.paintFlags = textView.paintFlags and Paint.STRIKE_THRU_TEXT_FLAG.inv()
     }
+
+    private fun strikeThruText(textView: TextView) {
+        textView.paintFlags = textView.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
+    }
+
+    inner class ViewHolder(mView: View ) : RecyclerView.ViewHolder(mView) {
+        private val checkBox: CheckBox = mView.check_box
+        private val todoTitleView: TextView = mView.todo_title
+
+        fun bind(todoItem: TodoItem, mListener: ListItemListener?) {
+            // Set listener to null so that the immediate isChecked does not trigger the listener
+            checkBox.setOnCheckedChangeListener(null)
+            checkBox.isChecked = todoItem.isDone
+            if (todoItem.isDone) strikeThruText(todoTitleView)
+            if (todoItem.isStarred) {
+                // TODO
+            }
+
+            checkBox.setOnCheckedChangeListener { _, isChecked ->
+                toggleTextStrikeThru(isChecked, todoTitleView)
+                mListener?.toggleDone(isChecked, todoItem)
+            }
+
+            todoTitleView.text = todoItem.title
+            todoTitleView.setOnClickListener(mDetailViewListener)
+        }
+    }
+
 }
